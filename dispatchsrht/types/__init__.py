ff0a179cd4c1df11aeb6def0c77494b94dd3d8a0
Drew DeVault <sir@cmpwn.com>
2018-12-30 18:29:23 -0500

    Remove __init__ from mixin-based classes

--- dispatchsrht/types/__init__.py
+++ dispatchsrht/types/__init__.py
@@ -2,7 +2,6 @@ from srht.database import Base
 from srht.oauth import ExternalUserMixin
 
 class User(Base, ExternalUserMixin):
-    def __init__(*args, **kwargs):
-        ExternalUserMixin.__init__(*args, **kwargs)
+    pass
 
 from dispatchsrht.types.task import Task
