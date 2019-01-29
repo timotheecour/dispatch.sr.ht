0907fcb01ec03211ee8a5a5c6e4b4ba228c1d543
Drew DeVault <sir@cmpwn.com>
2019-01-28 21:11:35 -0500

    Update setup.py to be more portable

--- setup.py
+++ setup.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python3
 from distutils.core import setup
 import subprocess
-import glob
 import os
+import site
+import sys
 
-subprocess.call(["make"])
+if hasattr(site, 'getsitepackages'):
+    pkg_dirs = site.getsitepackages()
+    if site.getusersitepackages():
+        pkg_dirs.append(site.getusersitepackages())
+    for pkg_dir in pkg_dirs:
+        srht_path = os.path.join(pkg_dir, "srht")
+        if os.path.isdir(srht_path):
+            break
+    else:
+        raise Exception("Can't find core srht module in your site packages "
+            "directories. Please install it first.")
+else:
+    srht_path = os.getenv("SRHT_PATH")
+    if not srht_path:
+        raise Exception("You're running inside a virtual environment. "
+            "Due to virtualenv limitations, you need to set the "
+            "$SRHT_PATH environment variable to the path of the "
+            "core srht module.")
+    elif not os.path.isdir(srht_path):
+        raise Exception(
+            "The $SRHT_PATH environment variable points to an invalid "
+            "directory: {}".format(srht_path))
+
+subp = subprocess.run(["make", "SRHT_PATH=" + srht_path])
+if subp.returncode != 0:
+    sys.exit(subp.returncode)
 
 ver = os.environ.get("PKGVER") or subprocess.run(['git', 'describe', '--tags'],
       stdout=subprocess.PIPE).stdout.decode().strip()
